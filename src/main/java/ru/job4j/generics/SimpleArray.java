53d8f6feef68abfbf69778476814349c738a3702
user <none>
2021-05-01 16:22:16 +0500

    5.2.1. Реализовать SimpleArray<T> [#489382]

---
+++ src/main/java/ru/job4j/generics/SimpleArray.java
@@ -0,0 +1,105 @@
+/*
+Создать класс:
+public class SimpleArray<T>
+Добавить методы:
+add(T model) - добавляет указанный элемент (model) в первую свободную ячейку;
+set(int index, T model) - заменяет указанным элементом (model) элемент, находящийся по индексу index;
+remove(int index) - удаляет элемент по указанному индексу, все находящиеся справа элементы при этом необходимо сдвинуть на единицу влево (в середине массива не должно быть пустых ячеек);
+get(int index) - возвращает элемент, расположенный по указанному индексу;
+Также, реализуйте интерфейс Iterable<T> - метод iterator() возвращает итератор, предназначенный для обхода данной структуры.
+Объект должен принимать количество ячеек. Структура не должна быть динамической.
+
+Примечание:
+1. В методах, где используется индекс нужно делать валидацию. Индекс должен находиться в рамках добавленных элементов. Например, у вас есть хранилище из 10 элементов. Вы добавили 3 элемента. Каким может быть индекс? [0, 2]. Для проверки индекса используйте метод Objects.checkIndex().
+Этот метод используется для проверки того, находится ли индекс в пределах заданной длины. Он возвращает индекс, если 0 <= index < length. В противном случае генерируется исключение IndexOutOfBoundsException.
+Обрабатывать исключение не нужно! Т.к. это связано не с нашей ошибкой, а с ошибкой пользователя нашего класса
+Objects.checkIndex(index, size);
+// Далее ваша логика
+2. Не путать null элементы и пустые ячейки контейнера. Список может содержать null элементы.
+3. Для удаления использовать System.arraycopy() вместо цикла
+*/
+
+package ru.job4j.generics;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SimpleArray<T> implements Iterable<T> {
+    private Object[] array;
+    private int size;
+
+    public SimpleArray() {
+        size = 0;
+        array = new Object[10];
+    }
+
+    private void extend() {
+        int newsize = 0;
+        if (array.length <= Integer.MAX_VALUE * 0.66) {
+            newsize = array.length + (array.length >> 1);
+        } else {
+            throw new ArrayIndexOutOfBoundsException("to much elements");
+        }
+        Object[] newArray = new Object[newsize];
+        System.arraycopy(array, 0, newArray, 0, size);
+        array = newArray;
+    }
+
+    private void compress(int index) {
+        if(index < size) {
+            System.arraycopy(array, index + 1, array, index, size - index);
+        } else {
+            System.arraycopy(array, index + 1, array, index, size - index);
+        }
+    }
+
+    public void add(T model) {
+        if (array.length < size + 5) {
+            extend();
+        }
+        array[size++] = model;
+
+    }
+
+    public void set(int index, T model) {
+        if (index >= 0 && index <= size) {
+            array[index] = model;
+        }
+    }
+
+    public void remove(int index) {
+        if
+        compress(index);
+        size--;
+    }
+
+    T get(int index) {
+        return (T) array[index];
+    }
+
+    int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        class SimpleArrayIter implements Iterator<T> {
+            private int index;
+
+            @Override
+            public boolean hasNext() {
+                return index < size;
+            }
+
+            @Override
+            public T next() {
+                if (hasNext()) {
+                    return (T) array[index++];
+                } else {
+                    throw new NoSuchElementException();
+                }
+            }
+        }
+        return new SimpleArrayIter();
+    }
+}
---
+++ src/test/java/ru/job4j/generics/SimpleArrayTest.java
@@ -0,0 +1,75 @@
+package ru.job4j.generics;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class SimpleArrayTest {
+    private SimpleArray<Integer> sa;
+
+    @Before
+    public void setUp() {
+        sa = new SimpleArray<>();
+        sa.add(3);
+        sa.add(5);
+        sa.add(6);
+    }
+
+    @Test
+    public void addAndGet() {
+       Assert.assertEquals(3, (int) sa.get(0));
+       Assert.assertEquals(5, (int) sa.get(1));
+       Assert.assertEquals(6, (int) sa.get(2));
+    }
+
+    @Test
+    public void set() {
+        sa.set(1, 8);
+        Assert.assertEquals(8, (int) sa.get(1));
+    }
+
+    @Test
+    public void remove() {
+        sa.add(9);
+        Assert.assertEquals(6, (int) sa.get(2));
+        sa.remove(0);
+        Assert.assertEquals(5, (int) sa.get(0));
+        Assert.assertEquals(6, (int) sa.get(1));
+        Assert.assertEquals(9, (int) sa.get(2));
+    }
+
+    @Test
+    public void size() {
+        assertEquals(3, sa.size());
+        sa.add(0);
+        assertEquals(4, sa.size());
+        sa.remove(3);
+        assertEquals(3, sa.size());
+    }
+
+    @Test
+    public void iterator() {
+        Iterator<Integer> iter = sa.iterator();
+        assertEquals(true, iter.hasNext());
+        assertEquals(3, (int) iter.next());
+        assertEquals(5, (int) iter.next());
+        assertEquals(6, (int) iter.next());
+    }
+
+    @Test
+    public void growCheck() {
+        for(int i = 0; i < 250; i++) {
+            sa.add(i);
+        }
+        for(; sa.size()>0;) {
+            sa.remove(sa.size());
+        }
+
+    }
+}
